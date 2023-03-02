730d8897aae4ff4351697a29c510a70f27a29793
UntalantedOne <105394292+UntalantedOne@users.noreply.github.com>
2023-03-02 17:31:44 +0100

    Added of solutions

--- 2_page_w_no_likes.sql
+++ 2_page_w_no_likes.sql
@@ -15,3 +15,28 @@ left join (select distinct page_id from page_likes) b
 on a.page_id = b.page_id
 where b.page_id is null
 order by a.page_id asc
+
+
+-- Solution 1:
+
+SELECT pages.page_id
+FROM pages
+LEFT OUTER JOIN page_likes AS likes
+  ON pages.page_id = likes.page_id
+WHERE likes.page_id IS NULL;
+
+-- Solution 2:
+SELECT page_id
+FROM pages
+WHERE NOT EXISTS (
+  SELECT 1
+  FROM page_likes AS likes
+  WHERE likes.page_id = pages.page_id);
+  
+  -- Solution 3:
+  SELECT page_id
+FROM pages
+EXCEPT
+SELECT page_id
+FROM page_likes
+ORDER BY page_id;
